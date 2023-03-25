de07b4c8bf9f5f1e653413299bbc38983ed19f2a
UnderVarela <under.varela@gmail.com>
2023-03-25 13:07:34 +0100

    Todo el codigo funcional. Refactorizada la funcion Calcular con Switch.

--- js/mainGPT2.js
+++ js/mainGPT2.js
@@ -1,5 +1,5 @@
 // Declarar variables globales
-let nombre, precio, diasCaducidad, total;
+let nombre, precio, diasCaducidad, total = 0;
 
 // Función para validar el formulario
 function validarForm() {
@@ -33,27 +33,50 @@ class Perecedero {
   }
 }
 
-// Definir la función calcular
+// // Definir la función calcular (SIN REFACTORIZAR)
+// function calcular(producto, cantidad) {
+//     if (producto instanceof Perecedero) {
+//       if (producto.diasParaCaducar == 3) {
+//         const descuento = producto.precio * cantidad * 0.25;
+//         const precioFinal = producto.precio * cantidad - descuento;
+//         return {descuento: descuento, precioFinal: precioFinal};
+//       } else if (producto.diasParaCaducar == 2) {
+//         const descuento = producto.precio * cantidad * 0.33;
+//         const precioFinal = producto.precio * cantidad - descuento;
+//         return {descuento: descuento, precioFinal: precioFinal};
+//       } else if (producto.diasParaCaducar == 1) {
+//         const descuento = producto.precio * cantidad * 0.5;
+//         const precioFinal = producto.precio * cantidad - descuento;
+//         return {descuento: descuento, precioFinal: precioFinal};
+//       }
+//     }
+//     const precioFinal = producto.precio * cantidad;
+//     return {descuento: 0, precioFinal: precioFinal};
+//   }
+  
+// Definir la función calcular (REFACTORIZADA con SWITCH)
 function calcular(producto, cantidad) {
-  if (producto instanceof Perecedero) {
-    if (producto.diasParaCaducar == 3) {
-      const descuento = producto.precio * cantidad * 0.25;
-      const precioFinal = producto.precio * cantidad - descuento;
-      return {descuento: descuento, precioFinal: precioFinal};
-    } else if (producto.diasParaCaducar == 2) {
-      const descuento = producto.precio * cantidad * 0.33;
-      const precioFinal = producto.precio * cantidad - descuento;
-      return {descuento: descuento, precioFinal: precioFinal};
-    } else if (producto.diasParaCaducar == 1) {
-      const descuento = producto.precio * cantidad * 0.5;
-      const precioFinal = producto.precio * cantidad - descuento;
-      return {descuento: descuento, precioFinal: precioFinal};
+    let descuento = 0;
+    let precioFinal = producto.precio * cantidad;
+  
+    if (producto instanceof Perecedero) {
+      switch (producto.diasParaCaducar) {
+        case 3:
+          descuento = precioFinal * 0.25;
+          break;
+        case 2:
+          descuento = precioFinal * 0.33;
+          break;
+        case 1:
+          descuento = precioFinal * 0.5;
+          break;
+      }
+      precioFinal -= descuento;
     }
+  
+    return { descuento: descuento, precioFinal: precioFinal };
   }
-  const precioFinal = producto.precio * cantidad;
-  return {descuento: 0, precioFinal: precioFinal};
-}
-// Agregar evento al submit
+  // Agregar evento al submit
 const form = document.querySelector('#formulario');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -67,36 +90,34 @@ form.addEventListener('submit', (event) => {
     // Crear una instancia de la clase Perecedero o simplemente usar el precio si el valor de diasCaducidad es 0
     let producto;
     if (diasCaducidad !== "0") {
-      producto = new Perecedero(precio, diasCaducidad);
+      producto = new Perecedero(parseFloat(precio), parseInt(diasCaducidad));
     } else {
-      producto = { precio: precio };
+      producto = { precio: parseFloat(precio) };
     }
 
     // Llamar a la función calcular y agregar el resultado a la tabla
     const cantidad = 1; // Cantidad por defecto
     const precioCalculado = calcular(producto, cantidad);
-    if (!isNaN(precioCalculado)) {
+    if (!isNaN(precioCalculado.precioFinal)) {
       total = total || 0;
-      total += precioCalculado;
+      total += precioCalculado.precioFinal;
       const totalElem = document.querySelector('#total');
       totalElem.innerHTML = `<strong>${total.toFixed(2)}€</strong>`;
 
-      
       let descuento;
       let pvp;
       if (diasCaducidad !== "0") {
-        descuento = precio - precioCalculado;
-        pvp = precioCalculado.toFixed(2);
+        descuento = precioCalculado.descuento;
+        pvp = precioCalculado.precioFinal.toFixed(2);
       } else {
         descuento = 0;
-        pvp = precio;
+        pvp = parseFloat(precio).toFixed(2);
       }
 
-      const row = `<tr><td>${nombre}</td><td>${precio}€</td><td>${diasCaducidad}</td><td>${descuento.toFixed(2)}</td><td>${pvp}€</td></tr>`;
+      const row = `<tr><td>${nombre}</td><td>${parseFloat(precio).toFixed(2)}€</td><td>${diasCaducidad}</td><td>${descuento.toFixed(2)}</td><td>${pvp}€</td></tr>`;
       tbody.innerHTML += row;
-          
-          }
     }
+  }
 });
 
 const btnLimpiar = document.querySelector('#limpiar');
@@ -106,4 +127,3 @@ btnLimpiar.addEventListener('click', (event) => {
   document.querySelector('#precio').value = '';
   document.querySelector('select[name="select"]').selectedIndex = 0;
 });
-
